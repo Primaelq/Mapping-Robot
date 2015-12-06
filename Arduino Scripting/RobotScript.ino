926be67f5a225bba825c1639e71a4c4fdfec09c2
Primaelq <primaelq@gmail.com>
2015-12-06 12:46:11 +0100

    Updated RobotScript
    Forgot to push RobotScript in the last commit, the robot can now be controlled by bluetooth and see obstacles with an ultrasonic sensor

--- Arduino Scripting/RobotScript.ino
+++ Arduino Scripting/RobotScript.ino
@@ -1,6 +1,8 @@
-
+#include <SoftwareSerial.h>
 #include <AFMotor.h>
 #include <Servo.h>
+
+//SoftwareSerial BTSerial (19, 18); // Rx, Tx
 
 const int trigPin = 39;
 const int echoPin = 45;
@@ -14,18 +16,31 @@ long obstacleDistance;
 long lecture_echo;
 
 int i = 0;
+int instruction = 0;
 
 bool turningRight = false;
 
+enum State
+{
+  forward,
+  backward,
+  right,
+  left,
+  stand
+};
+
+State state;
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin (9600);
+  Serial1.begin (9600);
 
   servo1.attach(9);
-  
+
   pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
-  pinMode(echoPin, INPUT); 
+  pinMode(echoPin, INPUT);
 
   motorRight.setSpeed (150);
   motorRight.run (RELEASE);
@@ -33,17 +48,16 @@ void setup()
   motorLeft.setSpeed (150);
   motorLeft.run (RELEASE);
 
-  servo1.attach(9);
+  state = stand;
 }
 
 void loop()
 {
+  /*DistanceCalculation ();
 
-  DistanceCalculation ();
-  
   motorRight.run (FORWARD);
   motorLeft.run (FORWARD);
-  
+
   if (i <= 50)
   {
     turningRight = true;
@@ -62,8 +76,6 @@ void loop()
     LookLeft ();
   }
 
-  Serial.print (i);
-  
   servo1.write (i);
     
   if (obstacleDistance < 20 && i > 40)
@@ -75,19 +87,95 @@ void loop()
     TurnRight ();
   }
   
-  delay (5);
+  delay (5);*/
+  
+  if (Serial1.available () > 0)
+  {
+    //Serial.write (Serial1.read ());
+    instruction = Serial1.parseInt ();
+  }
+  
+  if(Serial.available () > 0)
+  {
+    Serial1.write (Serial.read ());
+  }
+
+  switch (instruction)
+  {
+    case 1:
+      state = forward;
+      break;
+      
+    case 2:
+      state = backward;
+      break;
+      
+    case 3:
+      state = right;
+      break;
+      
+    case 4:
+      state = left;
+      break;
+      
+    case 5:
+      state = 0stand;
+      break;
+
+    default:
+      state = stand;
+      break;
+  }
+
+  Serial.println (instruction);
+  Serial1.write (state);
+
+  switch (state)
+  {
+    case forward:
+      GoForward ();
+      break;
+
+    case backward:
+      GoBackward ();
+      break;
+
+    case right:
+      TurnRight ();
+      break;
+
+    case left:
+      TurnLeft ();
+      break;
+
+    case stand:
+      Stand ();
+  }
+  
 }
 
-// Turn Servo
+// Turn Servo Right
 void LookRight ()
 {
   i++;
 }
 
-// Turn Servo
+// Turn Servo Left
 void LookLeft ()
 {
   i--;
+}
+
+void GoForward ()
+{
+  motorRight.run (FORWARD);
+  motorLeft.run (FORWARD);
+}
+
+void GoBackward ()
+{
+  motorRight.run (BACKWARD);
+  motorLeft.run (BACKWARD);
 }
 
 void TurnRight ()
@@ -102,12 +190,18 @@ void TurnLeft ()
   motorLeft.run (BACKWARD);
 }
 
-void DistanceCalculation ()
+void Stand ()
 {
-  digitalWrite(trigPin, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW); 
-  lecture_echo = pulseIn(echoPin, HIGH);
-  obstacleDistance = lecture_echo / 58;
+  motorRight.run (RELEASE);
+  motorLeft.run (RELEASE);
 }
 
+void DistanceCalculation ()
+{
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  lecture_echo = pulseIn(echoPin, HIGH);
+  obstacleDistance = lecture_echo / 58;
+  Serial1.write (obstacleDistance);
+}
